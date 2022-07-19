3edc519d346346e500342cd150460f57730589a4
Maruf Rien <maruf14222@gmail.com>
2022-07-19 18:13:23 +0600

    fix: recive values from reg form

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,47 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [isSupervisor, setIsSupervisor] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, password, confirmPassword, isSupervisor);
+  };
+
+  const handleCheckbox = () => {
+    setIsSupervisor(!isSupervisor);
+  };
+
   return (
-    <div class="container">
-      <h1>Registration form</h1>
-      <div>
-        <form>
-          <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">
-              Email address
-            </label>
-            <input
-              type="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-            />
-            <div id="emailHelp" class="form-text">
-              We'll never share your email with anyone else.
-            </div>
-          </div>
-          <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">
-              Password
-            </label>
-            <input
-              type="password"
-              class="form-control"
-              id="exampleInputPassword1"
-            />
-          </div>
-          <div class="mb-3 form-check">
-            <input
-              type="checkbox"
-              class="form-check-input"
-              id="exampleCheck1"
-            />
-            <label class="form-check-label" for="exampleCheck1">
-              Check me out
-            </label>
-          </div>
-          <button type="submit" class="btn btn-primary">
-            Submit
+    <div className="container">
+      <div className="register">
+        <form onSubmit={handleSubmit}>
+          <span className="formTitle">Lama Login</span>
+          <input
+            type="text"
+            placeholder="name"
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="confirmPassword"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <input
+            type="checkbox"
+            placeholder="isSupervisor"
+            onChange={handleCheckbox}
+            checked={isSupervisor}
+          />
+          <button type="submit" className="submitButton">
+            Login
           </button>
         </form>
       </div>
