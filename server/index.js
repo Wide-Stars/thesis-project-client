98f7359d15ab35b198bf08adeb632821c56b6d5a
Maruf Rien <maruf14222@gmail.com>
2022-07-19 12:59:46 +0600

    feat: Add jwt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-import express from "express"
+import express, { response } from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+import jwt from "jsonwebtoken"
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -13,15 +14,42 @@ const app = express();
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use("/user", userRoutes);
-app.use("/post", postRoutes);
-// Connect to MongoDB
-// mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-// 	console.log("Successfully connected to MongoDB");
-// });
+const users = [
+	{
+		id: 1,
+		name: "jon doe",
+		password: "jon1122",
+		isSupervisor: true,
+	},
+	{
+
+
+		id: 2,
+		name: "Maruf Rien",
+		password: "asdf",
+		isSupervisor: false,
+	}
+]
+
+app.post("/", (req, res) => {
+	const { name, password } = req.body;
+
+	const user = users.find(u => u.name === name && u.password === password);
+	if (user) {
+		//generate token
+		const token = jwt.sign({ id: user.id, isSupervisor: user.isSupervisor }, process.env.JWT_SECRET);
+
+		// send response
+		res.json({ ...user, token })
+	} else {
+		res.status(400).json({ message: "Invalid username or password" })
+	}
+
+
+})
+
+
+
 
 
 
