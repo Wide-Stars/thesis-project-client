1371553c2b99afd14260f9e4b3e9f8a48a91aeca
Maruf Rien <maruf14222@gmail.com>
2022-07-20 13:15:03 +0600

    refactor!: add bootstrap to login and reg form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
+import '../styles/login.css';
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -15,20 +15,42 @@ const Login = () => {
     console.log(res);
   };
   return (
-    <div className="container">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <div class="signup-form">
+            <form action="" class="mt-5 border p-4 bg-light shadow">
+              <h4 class="mb-5 text-secondary">Login To Your Account</h4>
+              <div class="row">
+                <div class="mb-3 col-md-12">
+                  <label>Email</label>
+                  <input
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="Enter Email"
+                  />
+                </div>
+                <div class="mb-3 col-md-12">
+                  <label>Password</label>
+                  <input
+                    type="password"
+                    name="password"
+                    class="form-control"
+                    placeholder="Enter Password"
+                  />
+                </div>
+                <div class="col-md-12">
+                  <button class="btn btn-primary float-end">Login</button>
+                </div>
+              </div>
+            </form>
+            <p class="text-center mt-3 text-secondary">
+              Don't have a account? <a href="#">Register</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -24,40 +24,80 @@ const Register = () => {
   };
 
   return (
-    <div className="container">
-      <div className="register">
-        <form onSubmit={handleSubmit}>
-          <span className="formTitle">Login form</span>
-          <input
-            type="text"
-            placeholder="name"
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="email"
-            placeholder="email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="confirmPassword"
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <input
-            type="checkbox"
-            placeholder="isSupervisor"
-            onChange={handleCheckbox}
-            checked={isSupervisor}
-          />
-          <button type="submit" className="submitButton">
-            Login
-          </button>
-        </form>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <div class="signup-form">
+            <form action="" class="mt-5 border p-4 bg-light shadow">
+              <h4 class="mb-5 text-secondary">Create Your Account</h4>
+              <div class="row">
+                <div class="mb-3 col-md-12">
+                  <label>
+                    Name<span class="text-danger">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    class="form-control"
+                    placeholder="Enter Name"
+                  />
+                </div>
+
+                <div class="mb-3 col-md-12">
+                  <label>
+                    Email<span class="text-danger">*</span>
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    class="form-control"
+                    placeholder="Enter Email"
+                  />
+                </div>
+
+                <div class="mb-3 col-md-12">
+                  <label>
+                    Password<span class="text-danger">*</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    class="form-control"
+                    placeholder="Enter Password"
+                  />
+                </div>
+                <div class="mb-3 col-md-12">
+                  <label>
+                    Confirm Password<span class="text-danger">*</span>
+                  </label>
+                  <input
+                    type="password"
+                    name="confirmpassword"
+                    class="form-control"
+                    placeholder="Confirm Password"
+                  />
+                </div>
+                <div class="form-check ">
+                  <input
+                    class="form-check-input ml-2"
+                    type="checkbox"
+                    value=""
+                    id="flexCheckDefault"
+                  />
+                  <label class="form-check-label" for="flexCheckDefault">
+                    Default checkbox
+                  </label>
+                </div>
+                <div class="col-md-12">
+                  <button class="btn btn-primary float-end">Signup Now</button>
+                </div>
+              </div>
+            </form>
+            <p class="text-center mt-3 text-secondary">
+              If you have account, Please <a href="#">Login Now</a>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
