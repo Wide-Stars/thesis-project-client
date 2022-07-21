4fd0da876c1c565e0b08384515e212f3c5193d24
Maruf Rien <maruf14222@gmail.com>
2022-07-21 18:32:04 +0600

    refactor: fix reg page to sumit form data to db

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,46 +30,46 @@ const Login = () => {
     // }
   };
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 offset-md-3">
-          <div class="signup-form">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="signup-form">
             <form
               onSubmit={handleSubmit(onSubmit)}
-              class="mt-5 border p-4 bg-light shadow"
+              className="mt-5 border p-4 bg-light shadow"
             >
-              <h4 class="mb-5 text-secondary">Login To Your Account</h4>
-              <div class="row">
-                <div class="mb-3 col-md-12">
+              <h4 className="mb-5 text-secondary">Login To Your Account</h4>
+              <div className="row">
+                <div className="mb-3 col-md-12">
                   <label>Email</label>
                   <input
                     type="email"
                     name="email"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Enter Email"
                     {...register('email')}
                   />
                 </div>
                 <p className="wrn">{errors.email?.message}</p>
-                <div class="mb-3 col-md-12">
+                <div className="mb-3 col-md-12">
                   <label>Password</label>
                   <input
                     {...register('password')}
                     type="password"
                     name="password"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Enter Password"
                   />
                   <p className="wrn">{errors.password?.message}</p>
                 </div>
-                <div class="col-md-12">
-                  <button class="btn btn-primary float-end" type="submit">
+                <div className="col-md-12">
+                  <button className="btn btn-primary float-end" type="submit">
                     Login
                   </button>
                 </div>
               </div>
             </form>
-            <p class="text-center mt-3 text-secondary">
+            <p className="text-center mt-3 text-secondary">
               Don't have a account?
               <Link to="/register">Register Now</Link>
             </p>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import schema from '../components/Form';
@@ -7,6 +7,7 @@ import schema from '../components/Form';
 import '../styles/extra.css';
 
 const Register = () => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -16,107 +17,110 @@ const Register = () => {
     resolver: yupResolver(schema),
   });
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
     console.log({ data });
-    // e.preventDefault();
-    // const res = await axios.post('http://localhost:3000/api/user/create', {
-    //   name: name,
-    //   password: password,
-    //   isSupervisor: isSupervisor,
-    //   email: email,
-    // });
-    // const token = res.data.payload;
-    // navigate('/');
-    // // save token to local storage
-    // localStorage.setItem('token', token);
-  };
 
-  const handleCheckbox = () => {
-    setIsSupervisor(!isSupervisor);
+    const res = await axios.post('http://localhost:3000/api/user/create', {
+      name: data.name,
+      password: data.password,
+      isSupervisor: data.isSupervisor,
+      email: data.email,
+    });
+    const token = res.data.payload;
+    console.log(token);
+    // save token to local storage
+    localStorage.setItem('token', token);
+
+    navigate('/');
   };
 
   return (
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 offset-md-3">
-          <div class="signup-form">
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <div className="signup-form">
             <form
               onSubmit={handleSubmit(onSubmit)}
-              class="mt-5 border p-4 bg-light shadow"
+              className="mt-5 border p-4 bg-light shadow"
             >
-              <h4 class="mb-5 text-secondary">Create Your Account</h4>
-              <div class="row">
-                <div class="mb-3 col-md-12">
+              <h4 className="mb-5 text-secondary">Create Your Account</h4>
+              <div className="row">
+                <div className="mb-3 col-md-12">
                   <label>
-                    Name<span class="text-danger">*</span>
+                    Name<span className="text-danger">*</span>
                   </label>
                   <input
                     type="text"
                     name="name"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Enter Name"
                     {...register('name')}
                   />
                   <p className="wrn">{errors.name?.message}</p>
                 </div>
 
-                <div class="mb-3 col-md-12">
+                <div className="mb-3 col-md-12">
                   <label>
-                    Email<span class="text-danger">*</span>
+                    Email<span className="text-danger">*</span>
                   </label>
                   <input
                     type="email"
                     name="email"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Enter Email"
                     {...register('email')}
                   />
                   <p className="wrn">{errors.email?.message}</p>
                 </div>
 
-                <div class="mb-3 col-md-12">
+                <div className="mb-3 col-md-12">
                   <label>
-                    Password<span class="text-danger">*</span>
+                    Password<span className="text-danger">*</span>
                   </label>
                   <input
                     type="password"
                     name="password"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Enter Password"
                     {...register('password')}
                   />
                   <p className="wrn">{errors.password?.message}</p>
                 </div>
-                <div class="mb-3 col-md-12">
+                <div className="mb-3 col-md-12">
                   <label>
-                    Confirm Password<span class="text-danger">*</span>
+                    Confirm Password<span className="text-danger">*</span>
                   </label>
                   <input
                     type="password"
                     name="confirm-password"
-                    class="form-control"
+                    className="form-control"
                     placeholder="Confirm Password"
                     {...register('confirmPassword')}
                   />
                   <p className="wrn">{errors.confirmPassword?.message}</p>
                 </div>
-                <div class="form-check ">
+                <div className="form-check ">
                   <input
-                    class="form-check-input ml-2"
+                    className="form-check-input ml-2"
                     type="checkbox"
                     id="isSupervisor"
                     {...register('isSupervisor')}
                   />
-                  <label class="form-check-label" for="flexCheckDefault">
+                  <label
+                    className="form-check-label"
+                    htmlFor="flexCheckDefault"
+                  >
                     Supervisor Account
                   </label>
                 </div>
-                <div class="col-md-12">
-                  <button class="btn btn-primary float-end">Signup Now</button>
+                <div className="col-md-12">
+                  <button className="btn btn-primary float-end">
+                    Signup Now
+                  </button>
                 </div>
               </div>
             </form>
-            <p class="text-center mt-3 text-secondary">
+            <p className="text-center mt-3 text-secondary">
               If you have account, Please <Link to="/login">Login Now</Link>
             </p>
           </div>
