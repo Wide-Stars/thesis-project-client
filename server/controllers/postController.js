c73ad6e85390a5a702664c8bd4a0751919064ca1
Maruf Rien <maruf14222@gmail.com>
2022-07-22 00:55:01 +0600

    refactor: remove server code

--- server/controllers/postController.js
+++
@@ -1,3 +0,0 @@
-export const createPost = (req, res) => res.send("post created")
-export const modifyPost = (req, res) => res.send("post modified")
-export const removePost = (req, res) => res.send("post removed")
