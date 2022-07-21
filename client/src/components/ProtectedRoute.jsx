437c405156cd94fe0107881fd3d1c4e72191a730
Maruf Rien <maruf14222@gmail.com>
2022-07-21 11:34:59 +0600

    feat: add function to redirect after reg

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import axios from 'axios';
 
 const ProtectedRoute = () => {
-  const auth = false;
+  let auth = true;
+  const token = useSelector((state) => state.user);
+  // if(!token) {
+  //   auth = false
+  // } else{
+
+  // }
+  console.log(token);
 
   return auth ? <Outlet /> : <Navigate to="/login" />;
 };
