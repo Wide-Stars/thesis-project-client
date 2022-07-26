e7e25099e82a2f5b7e9e728f92f3da0434b4003b
hadiul-islam <hadiulislamofficial@gmail.com>
2022-07-26 22:05:13 +0600

    change design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,6 +6,7 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { schemaLogin } from '../components/Form';
 
 import '../styles/extra.css';
+import loginImg from '../assets/images/login.png'
 
 const Login = () => {
   const navigate = useNavigate();
@@ -34,47 +35,45 @@ const Login = () => {
     }
   };
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-6 offset-md-3">
-          <div className="signup-form">
-            <form
-              className="mt-5 border p-4 bg-light shadow"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <h4 className="mb-5 text-secondary">Login To Your Account</h4>
-              <div className="row">
-                <div className="mb-3 col-md-12">
-                  <label>Email</label>
-                  <input
-                    type="email"
-                    name="email"
-                    className="form-control"
-                    placeholder="Enter Email"
-                    {...register('email')}
-                  />
-                </div>
-                <p className="wrn">{errors.email?.message}</p>
-                <div className="mb-3 col-md-12">
-                  <label>Password</label>
-                  <input
-                    type="password"
-                    name="password"
-                    className="form-control"
-                    placeholder="Enter Password"
-                    {...register('password')}
-                  />
-                  <p className="wrn">{errors.password?.message}</p>
-                </div>
-                <div className="col-md-12">
-                  <button className="btn btn-primary float-end">Login</button>
-                </div>
+    <div className="secondary-bg vh-100">
+      <div className="container">
+        <div className="row py-5">
+          <div className="col-md-6 py-5">
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <h1 className='text-center mb-5'><span className='primary-highlighter'>Login </span><span className='text-light'>To Your Account</span></h1>
+              <div className="mb-3 col-md-12">
+                <label className='mb-3 text-light'>Enter yout email</label>
+                <input
+                  type="email"
+                  name="email"
+                  className="form-control "
+                  placeholder="Enter Email"
+                  {...register('email')}
+                />
+              </div>
+              <p className="wrn">{errors.email?.message}</p>
+              <div className="mb-3 col-md-12">
+                <label className='mb-3 text-light'>Enter your password</label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control "
+                  placeholder="Enter Password"
+                  {...register('password')}
+                />
+                <p className="wrn">{errors.password?.message}</p>
+              </div>
+              <div className="col-md-12">
+                <button className="btn btn-primary float-end">Login</button>
               </div>
             </form>
             <p className="text-center mt-3 text-secondary">
               Don't have a account?
               <Link to="/register">Register Now</Link>
             </p>
+          </div>
+          <div className="col-md-6 py-5 text-center">
+             <img src={loginImg} height="70%" alt="loginImg" />
           </div>
         </div>
       </div>
