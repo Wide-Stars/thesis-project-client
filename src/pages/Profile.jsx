b023043592876c5382c996d071e8ba08417018bf
Maruf Rien <maruf14222@gmail.com>
2022-08-08 18:18:41 +0600

    refactor: profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -56,182 +56,41 @@ const Profile = () => {
                 </div>
               </div>
             </div>
-            {/* <div className="card mt-3">
-              <ul className="list-group list-group-flush">
-                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-globe mr-2 icon-inline mr-5 "
-                    >
-                      <circle cx={12} cy={12} r={10} />
-                      <line x1={2} y1={12} x2={22} y2={12} />
-                      <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z" />
-                    </svg>
-                    <span className="m-2">Website</span>
-                  </h6>
-                  <span className="text-secondary">{userInfo?.name}.com</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-github mr-2 icon-inline"
-                    >
-                      <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
-                    </svg>
-                    <span className="m-2">Github</span>
-                  </h6>
-                  <span className="text-secondary">{userInfo?.name}</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-twitter mr-2 icon-inline text-info"
-                    >
-                      <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
-                    </svg>
-                    <span className="m-2">Twitter</span>
-                  </h6>
-                  <span className="text-secondary">{userInfo?.name}y</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-instagram mr-2 icon-inline text-danger"
-                    >
-                      <rect x={2} y={2} width={20} height={20} rx={5} ry={5} />
-                      <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
-                      <line x1="17.5" y1="6.5" x2="17.51" y2="6.5" />
-                    </svg>
-                    <span className="m-2">Instagram</span>
-                  </h6>
-                  <span className="text-secondary">{userInfo?.name}</span>
-                </li>
-                <li className="list-group-item d-flex justify-content-between align-items-center flex-wrap">
-                  <h6 className="mb-0">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={24}
-                      height={24}
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-facebook mr-2 icon-inline text-primary"
-                    >
-                      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-                    </svg>
-                    <span className="m-2">Facebook</span>
-                  </h6>
-                  <span className="text-secondary">{userInfo?.name}</span>
-                </li>
-              </ul>
-            </div> */}
+
             <div className="card mt-3 ">
-              {/* <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                class="bi bi-person-circle "
-                viewBox="0 0 16 16"
-              >
-                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-                <path
-                  fill-rule="evenodd"
-                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
-                />
-              </svg> */}
-              <p className="m-3">
-                Account type:{' '}
-                {userInfo?.isSupervisor ? 'Supervisor' : 'Student'}
-              </p>
+              <div className="row text-left ">
+                <div className="col-sm-4 ">
+                  <h6 className="">Full Name</h6>
+                </div>
+                <div className="col-sm-7 text-secondary">{userInfo?.name}</div>
+              </div>
+              <hr />
+              <div className="row text-left ">
+                <div className="col-sm-4">
+                  <h6 className="">Email</h6>
+                </div>
+                <div className="col-sm-7 text-secondary">{userInfo?.email}</div>
+              </div>
+              <hr />
+
+              <div className="row text-left ">
+                <div className="col-sm-4">
+                  <h6 className="">Account type:</h6>
+                </div>
+                <div className="col-sm-7 text-secondary">
+                  {userInfo?.isSupervisor ? '  Supervisor' : '  Student'}
+                </div>
+              </div>
+              <hr />
+
+              <Link className="text-center mb-3" to={'/add-post'}>
+                <button type="button" class="btn btn-info">
+                  Create new post
+                </button>
+              </Link>
             </div>
           </div>
           <div className="col-md-8">
-            <div className="card mb-3">
-              <div className="card-body">
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Full Name</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">
-                    {userInfo?.name}
-                  </div>
-                </div>
-                <hr />
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Email</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">
-                    {userInfo?.email}
-                  </div>
-                </div>
-                <hr />
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Phone</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">(239) 816-9029</div>
-                </div>
-                <hr />
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Mobile</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">(320) 380-4539</div>
-                </div>
-                <hr />
-                <div className="row">
-                  <div className="col-sm-3">
-                    <h6 className="mb-0">Address</h6>
-                  </div>
-                  <div className="col-sm-9 text-secondary">
-                    Bay Area, San Francisco, CA
-                  </div>
-                </div>
-                <hr />
-              </div>
-            </div>
-
             {postData.length > 0 &&
               postData.map((data) => (
                 <div className="card mt-3" key={data._id}>
@@ -239,7 +98,8 @@ const Profile = () => {
                     <div className="row" key={data._id}>
                       <div className="post-list ">
                         <div className="row">
-                          <div className="col-sm-2">
+                          <div className="col-sm-3">
+                            {/* img */}
                             <div className="picture">
                               <img
                                 alt="Opt wizard thumbnail"
@@ -260,7 +120,7 @@ const Profile = () => {
                             <h1 className="mb-3">{data.title}</h1>
                             <p className="description">{data.content}</p>
                           </div>
-                          <div className="col-sm-4" data-no-turbolink="">
+                          <div className="col-sm-1" data-no-turbolink="">
                             <Link
                               className="btn btn-info btn-download btn-round pull-right makeLoading"
                               to={`/post/${data._id}`}
