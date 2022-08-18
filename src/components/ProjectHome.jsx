c4a9c40394fc437588c64aec71407e31f5a517ce
hadiul-islam <hadiulislamofficial@gmail.com>
2022-08-18 16:28:33 +0600

    home page t and p s

--- src/components/ProjectHome.jsx
+++ src/components/ProjectHome.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ProjectHome = () => {
     return (
         <div className="project-home">
-            <h2 className='text-center mb-5 main-title'>Thesis</h2>
+            <h2 className='text-center mb-5 main-title'>Project</h2>
             <div className="container">
                 <img src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80ph" alt="ph" />
             </div>
